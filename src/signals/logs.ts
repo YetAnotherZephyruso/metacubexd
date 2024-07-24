8a66830bcae2c5e06cab733a2a5be1e720ec5b0a
YetAnotherZephyruso <176294927+YetAnotherZephyruso@users.noreply.github.com>
2024-07-24 11:03:13 +0800

    feat: logs pause

---
+++ src/signals/logs.ts
@@ -0,0 +1,30 @@
+import { useWsRequest } from '~/signals'
+import { logLevel, logMaxRows } from '~/signals/config'
+import { Log } from '~/types'
+
+type LogWithSeq = Log & { seq: number }
+
+let seq = 1
+const [logs, setLogs] = createSignal<LogWithSeq[]>([])
+
+const logsData = useWsRequest<Log>('logs', { level: logLevel() })
+const [paused, setPaused] = createSignal(false)
+
+createEffect(() => {
+  const data = logsData()
+
+  if (!data || paused()) {
+    return
+  }
+
+  setLogs((logs) => [{ ...data, seq }, ...logs].slice(0, logMaxRows()))
+  seq++
+})
+
+export function useLogs() {
+  return {
+    logs,
+    paused,
+    setPaused,
+  }
+}
